e273082df127382902aad43a674aefdb68531033
Ashish <ashishbhatiam@gmail.com>
2022-07-18 22:06:14 +0530

    added query

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,13 @@
-const getAllProductsStatic = (req, res) => {
-    throw new Error('testing async error')
-    res.status(200).json({msg: 'All Products Testing'})
+const Product = require('../models/product')
+
+const getAllProductsStatic = async(req, res) => {
+    const products = await Product.find({ featured: true })
+    res.status(200).json({ products, total: products && products.length ? products.length : 0 })
 }
 
-const getAllProducts = (req, res) => {
-    res.status(200).json({msg: 'All Products'})
+const getAllProducts = async(req, res) => {
+    const products = await Product.find(req.query);
+    res.status(200).json({ products, total: products && products.length ? products.length : 0 })
 }
 
 module.exports = {
