5dfb9aea890b04ab48ff5439ee5afbff5bf32c0f
Ashish <ashishbhatiam@gmail.com>
2022-07-18 22:53:04 +0530

    added custom regex query

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,24 @@ const getAllProductsStatic = async(req, res) => {
 }
 
 const getAllProducts = async(req, res) => {
-    const products = await Product.find(req.query);
+    const { featured, name, price, company } = req.query;      /* Destructuring Method */
+    const queryObj = {}
+    if(featured){
+        queryObj.featured = featured
+    }
+    if(price){
+        queryObj.price = price
+    }
+    if(company){
+        queryObj.company = company
+    }
+    if(name){
+        queryObj.name = { $regex: name, $options: 'i'}
+    }
+
+    // const products = await Product.find(req.query); /* Simple Method */
+    
+    const products = await Product.find(queryObj);
     res.status(200).json({ products, total: products && products.length ? products.length : 0 })
 }
 
