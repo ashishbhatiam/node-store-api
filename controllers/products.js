ee2257bdfad8e46421bfbebfa8bb4cefdab1f870
Ashish <ashishbhatiam@gmail.com>
2022-07-18 23:24:02 +0530

    added sort

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async(req, res) => {
 }
 
 const getAllProducts = async(req, res) => {
-    const { featured, name, price, company } = req.query;      /* Destructuring Method */
+    const { featured, name, price, company, sort } = req.query;      /* Destructuring Method */
     const queryObj = {}
     if(featured){
         queryObj.featured = featured
@@ -22,8 +22,15 @@ const getAllProducts = async(req, res) => {
     }
 
     // const products = await Product.find(req.query); /* Simple Method */
-    
-    const products = await Product.find(queryObj);
+
+    let result = Product.find(queryObj);
+    if(sort){
+        const sortData = sort.split(',').join(' ')
+        result = result.sort(sortData)
+    } else{
+        result = result.sort('createdAt')
+    }
+    const products = await result
     res.status(200).json({ products, total: products && products.length ? products.length : 0 })
 }
 
