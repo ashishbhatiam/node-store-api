bd511585d51270154b22333cba06eeaf2a20deda
Ashish <ashishbhatiam@gmail.com>
2022-07-20 22:16:38 +0530

    added select fields

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async(req, res) => {
 }
 
 const getAllProducts = async(req, res) => {
-    const { featured, name, price, company, sort } = req.query;      /* Destructuring Method */
+    const { featured, name, price, company, sort, fields } = req.query;      /* Destructuring Method */
     const queryObj = {}
     if(featured){
         queryObj.featured = featured
@@ -23,13 +23,23 @@ const getAllProducts = async(req, res) => {
 
     // const products = await Product.find(req.query); /* Simple Method */
 
+    // Find / Search
     let result = Product.find(queryObj);
+    
+    // Sort 
     if(sort){
         const sortData = sort.split(',').join(' ')
         result = result.sort(sortData)
     } else{
         result = result.sort('createdAt')
     }
+
+    // Allowed / Visible Fields
+    if(fields){
+        const fieldsData = fields.split(',').join(' ')
+        result = result.select(fieldsData)
+    }
+
     const products = await result
     res.status(200).json({ products, total: products && products.length ? products.length : 0 })
 }
