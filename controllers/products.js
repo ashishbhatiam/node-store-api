68f5360a81e5c25e4a909f6a9cf183b6673a88ac
Ashish <ashishbhatiam@gmail.com>
2022-07-20 22:54:39 +0530

    added pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async(req, res) => {
-    const products = await Product.find({ featured: true })
+    const products = await Product.find({})
     res.status(200).json({ products, total: products && products.length ? products.length : 0 })
 }
 
@@ -39,9 +39,15 @@ const getAllProducts = async(req, res) => {
         const fieldsData = fields.split(',').join(' ')
         result = result.select(fieldsData)
     }
+    
+    // Pagination
+    const page = Number(req.query.page) || 1;
+    const pageLimit = Number(req.query.pageLimit) || 10;
+    const skip = (page - 1) * pageLimit;
+    result = result.limit(pageLimit).skip(skip)
 
     const products = await result
-    res.status(200).json({ products, total: products && products.length ? products.length : 0 })
+    res.status(200).json({ products, total: products.length })
 }
 
 module.exports = {
