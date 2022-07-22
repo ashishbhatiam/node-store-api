6a42e11a4185bf3ad83fe5b2d111bcb3bb2fa49b
Ashish <ashishbhatiam@gmail.com>
2022-07-23 00:06:19 +0530

    added numeric filter search functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async(req, res) => {
-    const products = await Product.find({})
-    res.status(200).json({ products, total: products && products.length ? products.length : 0 })
+    const products = await Product.find({price: {$gt: 59}}).sort('price').select('name price')
+    res.status(200).json({ products, total: products.length})
 }
 
 const getAllProducts = async(req, res) => {
-    const { featured, name, price, company, sort, fields } = req.query;      /* Destructuring Method */
+    const { featured, name, price, company, sort, fields, numericFilters } = req.query;      /* Destructuring Method */
     const queryObj = {}
     if(featured){
         queryObj.featured = featured
@@ -19,6 +19,25 @@ const getAllProducts = async(req, res) => {
     }
     if(name){
         queryObj.name = { $regex: name, $options: 'i'}
+    }
+
+    if(numericFilters){
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        };
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+        const options = ['price', 'rating'];
+        filters = filters.split(', ').forEach(item => {
+            const [field, operator, value] = item.split('-');
+            if(options.includes(field)){
+                queryObj[field] = { [operator]: Number(value) }
+            }
+        });
     }
 
     // const products = await Product.find(req.query); /* Simple Method */
