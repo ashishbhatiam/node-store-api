e988595a338d04b34eb8d81821e53a4bb1405d3f
Ashish <ashishbhatiam@gmail.com>
2022-07-24 14:23:18 +0530

    remove static route & controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,5 @@
 const Product = require("../models/product");
 
-const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ price: { $gt: 59 } })
-    .sort("price")
-    .select("name price");
-  res.status(200).json({ products, total: products.length });
-};
-
 const getAllProducts = async (req, res) => {
   const { featured, name, price, company, sort, fields, numericFilters } =
     req.query; /* Destructuring Method */
@@ -77,5 +70,4 @@ const getAllProducts = async (req, res) => {
 
 module.exports = {
   getAllProducts,
-  getAllProductsStatic,
 };
